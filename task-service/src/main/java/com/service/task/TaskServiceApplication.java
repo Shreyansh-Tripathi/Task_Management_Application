6858368d891fecb142651f02065f057a598d40e4
Shreyansh-Tripathi <shreyansht03@gmail.com>
2023-05-17 18:16:24 +0530

    added configurations of services

--- task-service/src/main/java/com/service/task/TaskServiceApplication.java
+++ task-service/src/main/java/com/service/task/TaskServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class TaskServiceApplication {
 
 	public static void main(String[] args) {
