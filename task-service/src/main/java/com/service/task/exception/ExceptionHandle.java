4e1d0eea620e73ba318cc46d875f068fa4fd5539
Shreyansh-Tripathi <shreyansht03@gmail.com>
2023-05-25 19:28:02 +0530

    updates in exception handler

--- task-service/src/main/java/com/service/task/exception/ExceptionHandle.java
+++ task-service/src/main/java/com/service/task/exception/ExceptionHandle.java
@@ -1,10 +1,12 @@
 package com.service.task.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.support.WebArgumentResolver;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.util.*;
@@ -18,7 +20,24 @@ public class ExceptionHandle extends ResponseEntityExceptionHandler {
 
         errorMap.put("Message:", exception.getMessage());
 
-        return new ResponseEntity<Map<String, Object>>(errorMap, HttpStatusCode.valueOf(400));
+        return new ResponseEntity<Map<String, Object>>(errorMap, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(value = NoSuchFieldException.class)
+    public ResponseEntity<Map<String, Object>> handleNoSuchElementExceptions(NoSuchElementException exception, WebRequest request){
+        Map<String,Object> errorMap=new LinkedHashMap<>();
+
+        errorMap.put("Message:", exception.getMessage()+ ". Please try with a different parameter.");
+
+        return new ResponseEntity<Map<String, Object>>(errorMap, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,Object> errorMap=new LinkedHashMap<>();
+
+        errorMap.put("Message:", "You are not using the correct Request Method.");
+
+        return new ResponseEntity<Object>(errorMap, HttpStatus.BAD_REQUEST);
+    }
 }
